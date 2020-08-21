d85c1c5c925dedf024bcfb9b53c9448dd373d39e
Erik Harding <erik@praekelt.org>
2020-08-21 11:02:46 +0200

    add more tests

--- base/tests/test_forms.py
+++ base/tests/test_forms.py
@@ -1,12 +1,215 @@
+import json
+from typing import Any, Dict, Optional, Text
+from urllib.parse import urlencode
+
 import pytest
+import respx
+from rasa_sdk import Tracker
 from rasa_sdk.events import Form, SlotSet
 from rasa_sdk.executor import CollectingDispatcher
 
-from base.actions.actions import TBCheckTermsForm
+import base.actions.actions
+from base.actions.actions import TBCheckForm, TBCheckProfileForm, TBCheckTermsForm
 from base.tests import utils
 
 
-class TestHealthCheckTermsForm:
+class TestTBCheckProfileForm:
+    def get_tracker_for_text_slot_with_message(
+        self, slot_name: Text, text: Text, msg: Optional[Dict[Text, Any]] = None
+    ):
+        msgs = [{"event": "user", "metadata": msg or dict()}]
+
+        return Tracker(
+            "default",
+            {"requested_slot": slot_name},
+            {"text": text},
+            msgs,
+            False,
+            None,
+            {},
+            "action_listen",
+        )
+
+    @pytest.mark.asyncio
+    async def test_validate_age(self):
+        form = TBCheckProfileForm()
+        dispatcher = CollectingDispatcher()
+
+        tracker = utils.get_tracker_for_number_slot_with_value(form, "age", "1")
+        events = await form.run(dispatcher=dispatcher, tracker=tracker, domain=None)
+        assert events == [SlotSet("age", "<18"), SlotSet("requested_slot", "gender")]
+
+        tracker = utils.get_tracker_for_number_slot_with_value(form, "age", "2")
+        events = await form.run(dispatcher=dispatcher, tracker=tracker, domain=None)
+        assert events == [SlotSet("age", "18-39"), SlotSet("requested_slot", "gender")]
+
+        tracker = utils.get_tracker_for_number_slot_with_value(form, "age", "3")
+        events = await form.run(dispatcher=dispatcher, tracker=tracker, domain=None)
+        assert events == [SlotSet("age", "40-65"), SlotSet("requested_slot", "gender")]
+
+        tracker = utils.get_tracker_for_number_slot_with_value(form, "age", "4")
+        events = await form.run(dispatcher=dispatcher, tracker=tracker, domain=None)
+        assert events == [SlotSet("age", ">65"), SlotSet("requested_slot", "gender")]
+
+        tracker = utils.get_tracker_for_number_slot_with_value(form, "age", ["2", "39"])
+        events = await form.run(dispatcher=dispatcher, tracker=tracker, domain=None)
+        assert events == [SlotSet("age", None), SlotSet("requested_slot", "age")]
+
+        tracker = utils.get_tracker_for_number_slot_with_value(
+            form, "age", "not a number"
+        )
+        events = await form.run(dispatcher=dispatcher, tracker=tracker, domain=None)
+        assert events == [SlotSet("age", None), SlotSet("requested_slot", "age")]
+
+    @pytest.mark.asyncio
+    async def test_validate_gender(self):
+        form = TBCheckProfileForm()
+        dispatcher = CollectingDispatcher()
+
+        tracker = utils.get_tracker_for_number_slot_with_value(
+            form, "gender", "1", {"age": "18-39"}
+        )
+        events = await form.run(dispatcher=dispatcher, tracker=tracker, domain=None)
+        assert events == [
+            SlotSet("gender", "MALE"),
+            SlotSet("requested_slot", "province"),
+        ]
+
+        tracker = utils.get_tracker_for_number_slot_with_value(
+            form, "gender", "2", {"age": "18-39"}
+        )
+        events = await form.run(dispatcher=dispatcher, tracker=tracker, domain=None)
+        assert events == [
+            SlotSet("gender", "FEMALE"),
+            SlotSet("requested_slot", "province"),
+        ]
+
+        tracker = utils.get_tracker_for_number_slot_with_value(
+            form, "gender", "3", {"age": "18-39"}
+        )
+        events = await form.run(dispatcher=dispatcher, tracker=tracker, domain=None)
+        assert events == [
+            SlotSet("gender", "OTHER"),
+            SlotSet("requested_slot", "province"),
+        ]
+
+        tracker = utils.get_tracker_for_number_slot_with_value(
+            form, "gender", "4", {"age": "18-39"}
+        )
+        events = await form.run(dispatcher=dispatcher, tracker=tracker, domain=None)
+        assert events == [
+            SlotSet("gender", "RATHER NOT SAY"),
+            SlotSet("requested_slot", "province"),
+        ]
+
+    @pytest.mark.asyncio
+    async def test_validate_province(self):
+        form = TBCheckProfileForm()
+        dispatcher = CollectingDispatcher()
+
+        i = 1
+        for p in ["ec", "fs", "gt", "nl", "lp", "mp", "nw", "nc", "wc"]:
+            tracker = utils.get_tracker_for_number_slot_with_value(
+                form, "province", str(i), {"age": "18-39", "gender": "MALE"}
+            )
+            events = await form.run(dispatcher=dispatcher, tracker=tracker, domain=None)
+            assert events == [
+                SlotSet("province", p),
+                SlotSet("requested_slot", "location"),
+            ]
+            i += 1
+
+    def test_format_location(self):
+        """
+        Ensures that the location pin is returned in ISO6709 format
+        """
+        assert TBCheckProfileForm.format_location(0, 0) == "+00+000/"
+        assert TBCheckProfileForm.format_location(-1, -1) == "-01-001/"
+        assert TBCheckProfileForm.format_location(1.234, -5.678) == "+01.234-005.678/"
+        assert TBCheckProfileForm.format_location(-12.34, 123.456) == "-12.34+123.456/"
+        assert (
+            TBCheckProfileForm.format_location(51.481845, 7.216236)
+            == "+51.481845+007.216236/"
+        )
+
+    @pytest.mark.asyncio
+    async def test_validate_location_pin(self):
+        """
+        If a location pin is sent, it should be stored
+        """
+        form = TBCheckProfileForm()
+
+        tracker = self.get_tracker_for_text_slot_with_message(
+            "location",
+            "",
+            {"type": "location", "location": {"latitude": 1.23, "longitude": 4.56}},
+        )
+        events = await form.validate(CollectingDispatcher(), tracker, {})
+        assert events == [
+            SlotSet("location", "GPS: 1.23, 4.56"),
+            SlotSet("location_coords", "+01.23+004.56/"),
+            SlotSet("location_confirm", "yes"),
+        ]
+
+    @pytest.mark.asyncio
+    async def test_validate_location_text(self):
+        """
+        If there's no google places API credentials, then just use the text
+        """
+        form = TBCheckProfileForm()
+
+        tracker = self.get_tracker_for_text_slot_with_message("location", "Cape Town",)
+
+        events = await form.validate(CollectingDispatcher(), tracker, {})
+        assert events == [
+            SlotSet("location", "Cape Town"),
+        ]
+
+    @respx.mock
+    @pytest.mark.asyncio
+    async def test_validate_location_google_places(self):
+        """
+        If there's are google places API credentials, then do a lookup
+        """
+        base.actions.actions.config.GOOGLE_PLACES_API_KEY = "test_key"
+        querystring = urlencode(
+            {
+                "key": "test_key",
+                "input": "Cape Town",
+                "language": "en",
+                "inputtype": "textquery",
+                "fields": "formatted_address,geometry",
+            }
+        )
+        request = respx.get(
+            "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?"
+            f"{querystring}",
+            content=json.dumps(
+                {
+                    "candidates": [
+                        {
+                            "formatted_address": "Cape Town, South Africa",
+                            "geometry": {"location": {"lat": 1.23, "lng": 4.56}},
+                        }
+                    ]
+                }
+            ),
+        )
+        form = TBCheckProfileForm()
+
+        tracker = self.get_tracker_for_text_slot_with_message("location", "Cape Town",)
+
+        events = await form.validate(CollectingDispatcher(), tracker, {})
+        assert events == [
+            SlotSet("location", "Cape Town, South Africa"),
+            SlotSet("city_location_coords", "+01.23+004.56/"),
+        ]
+        assert request.called
+
+        base.actions.actions.config.GOOGLE_PLACES_API_KEY = None
+
+
+class TestTBCheckTermsForm:
     @pytest.mark.asyncio
     async def test_validate_terms(self):
         form = TBCheckTermsForm()
@@ -24,3 +227,37 @@ class TestHealthCheckTermsForm:
         tracker = utils.get_tracker_for_slot_from_intent(form, "terms", "more")
         events = await form.run(dispatcher=dispatcher, tracker=tracker, domain=None)
         assert events == [SlotSet("terms", None), SlotSet("requested_slot", "terms")]
+
+
+class TestTBCheckForm:
+    @pytest.mark.asyncio
+    async def test_validate_cough(self):
+        form = TBCheckForm()
+        dispatcher = CollectingDispatcher()
+
+        tracker = utils.get_tracker_for_number_slot_with_value(
+            form, "symptoms_cough", "1", {}
+        )
+        events = await form.run(dispatcher=dispatcher, tracker=tracker, domain=None)
+        assert events == [
+            SlotSet("symptoms_cough", "no"),
+            SlotSet("requested_slot", "symptoms_fever"),
+        ]
+
+        tracker = utils.get_tracker_for_number_slot_with_value(
+            form, "symptoms_cough", "2", {}
+        )
+        events = await form.run(dispatcher=dispatcher, tracker=tracker, domain=None)
+        assert events == [
+            SlotSet("symptoms_cough", "yes lt 2weeks"),
+            SlotSet("requested_slot", "symptoms_fever"),
+        ]
+
+        tracker = utils.get_tracker_for_number_slot_with_value(
+            form, "symptoms_cough", "3", {}
+        )
+        events = await form.run(dispatcher=dispatcher, tracker=tracker, domain=None)
+        assert events == [
+            SlotSet("symptoms_cough", "yes gt 2weeks"),
+            SlotSet("requested_slot", "symptoms_fever"),
+        ]
