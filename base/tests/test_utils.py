4c5c0b0d48eb86b59d8351c4572d9b951343ddb8
Erik Harding <erik@praekelt.org>
2020-09-18 10:16:48 +0200

    Update unknown exposure logic

--- base/tests/test_utils.py
+++ base/tests/test_utils.py
@@ -41,3 +41,22 @@ def test_risk_level():
     data = {"symptoms_cough": "yes", "symptoms_fever": "no", "exposure": "yes"}
     risk = utils.get_risk_level(data)
     assert risk == "high"
+
+    # No cough, no symptoms, unknown exposure
+    data = {"symptoms_cough": "no", "symptoms_fever": "no", "exposure": "not sure"}
+    risk = utils.get_risk_level(data)
+    assert risk == "low"
+
+
+def test_risk_template():
+    data = {"exposure": "not sure"}
+    template = utils.get_risk_template("low", data)
+    assert template == "utter_risk_low_unknown_exposure"
+
+    data = {"exposure": "no"}
+    template = utils.get_risk_template("low", data)
+    assert template == "utter_risk_low"
+
+    data = {"exposure": "not sure"}
+    template = utils.get_risk_template("high", data)
+    assert template == "utter_risk_high"
